6579b9ed1a9301b54b964caa73d6bc39dd3af92b
Daphne Soegijono <d.soegijono@gmail.com>
2014-09-30 15:48:14 +0700

    Add Insertion Sort implementation
    - Ask user input for sort type

--- sorting/ArraySorting.java
+++ sorting/ArraySorting.java
@@ -1,18 +1,23 @@
+import java.util.Scanner;
+
 public class ArraySorting {
 
 // Bucket sort
-// Bubble sort
-// Insertion sort
 // Selection sort
 // Heapsort
 // Mergesort
 
 // quicksort
 
+  /**
+   * Bubble Sort
+   * Time complexity: Best case = O(n). Average case = O(n^2). Worst case = O(n^2).
+   * Space complexity: O(1).
+   */
   public static int[] bubbleSort(int[] arr) {
     int len = arr.length;
     boolean swapped = false;
-    
+
     do {
       swapped = false;
       for (int i = 1; i < len; i++) {
@@ -22,7 +27,7 @@ public class ArraySorting {
         }
       }
     } while (swapped == true);
-    
+
     return arr;
   }
 
@@ -35,25 +40,66 @@ public class ArraySorting {
 
     return buckets; //FIXME
   }
-  
+
+  /**
+   * Insertion Sort
+   * Time complexity: Best case = O(n). Average case = O(n^2). Worst case = O(n^2).
+   * Space complexity: O(1).
+   */
+  private static int[] insertionSort(int[] arr) {
+    int sorted = 1;
+    while (sorted < arr.length) {
+      for (int i = sorted; i > 0; i--) {
+        printArray(arr);
+        if (arr[i] < arr[i-1]) {
+          swap(arr, i, i-1);
+        } else {
+          break;
+        }
+      }
+      sorted++;
+    }
+    return arr;
+  }
+
+  public static void main(String[] args) {
+    int[] arr = { 10, 1, 23, 9, 19, 30, 24, -15 };
+    printArray(arr);
+    //TODO: User input for array?
+
+    System.out.println("Please select a sort algorithm:");
+    System.out.println(" 1. Bubble Sort");
+    System.out.println(" 2. Insertion Sort");
+
+    Scanner reader = new Scanner(System.in);
+    int x = reader.nextInt();
+    reader.close();
+
+    switch (x) {
+      case 1:
+        System.out.println("Performing bubble sort...");
+        printArray(bubbleSort(arr));
+        break;
+      case 2:
+        System.out.println("Performing insertion sort...");
+        printArray(insertionSort(arr));
+        break;
+      default:
+        System.out.println("Error. Invalid selection.");
+    }
+  }
+
+  private static void printArray(int[] arr) {
+    for (int i = 0; i < arr.length; i++) {
+      System.out.print(arr[i] + " ");
+    }
+    System.out.println();
+  }
+
   public static int[] swap(int[] arr, int i, int j) {
     int temp = arr[i];
     arr[i] = arr[j];
     arr[j] = temp;
     return arr;
-  }
-
-  public static void main(String[] args) {
-    //TODO use args to use diff sorts
-    int[] arr = {1, 23, 9, 19, 30, 24, 15};
-    printArray(arr);
-    printArray(bubbleSort(arr));
-  }
-  
-  private static void printArray(int[] arr) {
-    for (int i = 0; i < arr.length; i++) {
-      System.out.print(arr[i] + ", ");
-    }
-    System.out.println();
   }
 }
